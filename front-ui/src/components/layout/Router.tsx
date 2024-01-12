33a1cf575743cbdc5d9cc0551fd466e3d0e19151
benjaminchardin <bchardin@coreoz.com>
2024-01-12 16:55:35 +0100

    Added : - fix login pages style

--- front-ui/src/components/layout/Router.tsx
+++ front-ui/src/components/layout/Router.tsx
@@ -18,17 +18,19 @@ export default function Router() {
     <div className="page-layout">
       <Header/>
       <div className="main-layout">
-        <Routes>
-          <Route path={REGISTER} element={<Register />} />
-          <Route path={LOGIN} element={<Login />} />
-          <Route path={HOME} element={<Home />} />
-          <Route path={MENTIONS_LEGALES} element={<Legal page='mentions-legales' />} />
-          <Route path={RGPD} element={<Legal page='rgpd' />} />
-          <Route path={VERIFY_EMAIL} element={<VerifyEmail />} />
-          <Route path={CHANGE_PASSWORD} element={<ChangePassword />} />
-          <Route path={RESET_PASSWORD} element={<ResetPassword />} />
-          <Route path="*" element={<Navigate to={{ pathname: HOME }} />} />
-        </Routes>
+        <div className="content">
+          <Routes>
+            <Route path={REGISTER} element={<Register />} />
+            <Route path={LOGIN} element={<Login />} />
+            <Route path={HOME} element={<Home />} />
+            <Route path={MENTIONS_LEGALES} element={<Legal page='mentions-legales' />} />
+            <Route path={RGPD} element={<Legal page='rgpd' />} />
+            <Route path={VERIFY_EMAIL} element={<VerifyEmail />} />
+            <Route path={CHANGE_PASSWORD} element={<ChangePassword />} />
+            <Route path={RESET_PASSWORD} element={<ResetPassword />} />
+            <Route path="*" element={<Navigate to={{ pathname: HOME }} />} />
+          </Routes>
+        </div>
       </div>
       <Footer />
     </div>
